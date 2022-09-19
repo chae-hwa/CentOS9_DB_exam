4692cc9a664b061a15e02a8d1db54c245696e11a
chae-hwa <ihj0519@naver.com>
2022-09-19 11:12:08 +0900

    서버에서 빌드 성공을 위해 test 프로파일 추가

--- src/test/java/com/example/centos9_exam/CentOs9ExamApplicationTests.java
+++ src/test/java/com/example/centos9_exam/CentOs9ExamApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.centos9_exam;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CentOs9ExamApplicationTests {
 
     @Test
